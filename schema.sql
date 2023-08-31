9faba9fdacd1516f613d9a0288c7e16e3d7f5e37
Felipe Bento Ferreira <94194378+Fbgg2k@users.noreply.github.com>
2023-08-30 22:53:14 -0300

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE fornecedores (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    razao_social TEXT NOT NULL,
+    endereco TEXT NOT NULL,
+    contato_nome TEXT,
+    contato_email TEXT
+);
+
